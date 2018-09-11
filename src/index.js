6402693b9bea62ca467237252d1d8406c7557db4
Martin <martbelmoaw@gmail.com>
2018-09-11 15:35:52 +0300

    result 50%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if(str.length % 2 !== 0){
+        return false;
+    } else {
+        let chars = str.split(''),
+            stack = [],
+            open = [],
+            close = [],
+            closeIndex,
+            openIndex;
+
+
+        for (let br = 0; br < bracketsConfig.length; br++){
+            open.push(bracketsConfig[br][0]);
+            close.push(bracketsConfig[br][1]);
+        }
+
+        for (let i = 0; i < chars.length; i++) {
+            openIndex = open.indexOf(chars[i]);
+            if (openIndex !== -1) {
+                stack.push(openIndex);
+                continue;
+            }
+
+            console.log(stack);
+
+            closeIndex = close.indexOf(chars[i]);
+            if (closeIndex !== -1) {
+                openIndex = stack.pop();
+                if (closeIndex !== openIndex) {
+                    return false;
+                }
+            }
+        }
+
+        if (stack.length !== 0) {
+            return false;
+        }
+
+        return true;
+    }
 }
