d4349834cf4dd27b87ef5e0461ce54479ab6b300
Martin <martbelmoaw@gmail.com>
2018-09-24 20:32:33 +0300

    refactored

--- src/index.js
+++ src/index.js
@@ -2,60 +2,40 @@ module.exports = function check(str, bracketsConfig) {
     if(str.length % 2 !== 0){
         return false;
     } else {
-        let stack = [];
-        let map = {
+        let stack = [],
+            last = '';
+        const map = {
             '(': ')',
             '[': ']',
             '{': '}',
-            '|': '|',
             '1': '2',
             '3': '4',
             '5': '6',
+        };
+        const map_advanced = {
+            '|': '|',
             '7': '7',
             '8': '8'
-        }
+        };
 
         for (let i = 0; i < str.length; i++) {
-
-        // If character is an opening brace add it to a stack
-            if (str[i] === '(' || str[i] === '{' || str[i] === '[' || str[i] === '1' || str[i] === '3' || str[i] === '5') {
+            if (str[i] in map) {
                 stack.push(str[i]);
-            } else if (str[i] === '|'){
+            } else if (str[i] in map_advanced){
                 if(stack.indexOf(str[i]) === -1){
                     stack.push(str[i]);
                 } else {
-                    let last = stack.pop();
+                    last = stack.pop();
 
-                    if (str[i] !== map[last]) {return false};
+                    if (str[i] !== map_advanced[last]) return false;
                 }
-            } else if (str[i] === '7'){
-                if(stack.indexOf(str[i]) === -1){
-                    stack.push(str[i]);
-                } else {
-                    let last = stack.pop();
+            } else {
+                last = stack.pop();
 
-                    if (str[i] !== map[last]) {return false};
-                }
-            } else if (str[i] === '8'){
-                if(stack.indexOf(str[i]) === -1){
-                    stack.push(str[i]);
-                } else {
-                    let last = stack.pop();
-
-                    if (str[i] !== map[last]) {return false};
-                }
-            }
-        //  If that character is a closing brace, pop from the stack, which will also reduce the length of the stack each time a closing bracket is encountered.
-            else {
-                let last = stack.pop();
-
-            //If the popped element from the stack, which is the last opening brace doesn’t match the corresponding closing brace in the map, then return false
-                if (str[i] !== map[last]) {return false};
+                if (str[i] !== map[last]) return false;
             }
         }
-    // By the completion of the for loop after checking all the brackets of the str, at the end, if the stack is not empty then fail
-            if (stack.length !== 0) {return false};
 
-        return true;
+        return stack.length === 0;
     }
 }
